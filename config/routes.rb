297590130d68e0698f23a895a8823db67b2e924d
Tyler Porter <hello@cleverladder.com>
2015-11-22 13:18:30 -0600

    Edit user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :admins, controllers: {registrations: 'registrations'}
+  devise_for :admins, controllers: {registrations: 'registrations'}, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'user/edit'}
 	resources :beers do 
 		collection do 
 			get 'search'
